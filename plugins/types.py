77dce6e6ac8288a8f2e3999d9d95d430662e4779
Mathieu Tarral <mathieu.tarral@protonmail.com>
2024-04-08 22:25:44 +0200

    types: add unique constraints interface

--- plugins/types.py
+++ plugins/types.py
@@ -4,11 +4,17 @@ import logging
 import tempfile
 from abc import abstractmethod
 from contextlib import contextmanager
-from typing import Any
+from typing import Any, List
 
 from attrs import Factory, define, field
 from neogit.model.neo import Commit
 from neogit.service import Neogit
+
+
+@define(auto_attribs=True)
+class UniqueConstraint:
+    label: str = field()
+    property_list: list[str] = field()
 
 
 @define(auto_attribs=True)
@@ -24,8 +30,22 @@ class AbstractPlugin:
 
     def __call__(self, commit: Commit, *args: Any, **kwds: Any) -> Any:
         """Execute the run method inside a neomodel transaction"""
+        # can't mix schema modification and write query in the same transaction
+        with self.neogit.db.transaction:
+            self.ensure_constraints()
         with self.neogit.db.transaction:
             self.run(commit)
+
+    def ensure_constraints(self):
+        """Ensure the constraints are in the database"""
+        for constraint in self.constraints_data():
+            for prop in constraint.property_list:
+                query = f"CREATE CONSTRAINT IF NOT EXISTS FOR (n:{constraint.label}) REQUIRE n.{prop} IS UNIQUE"
+                self.neogit.db.cypher_query(query)
+
+    def constraints_data(self) -> List[UniqueConstraint]:
+        """Return the constraints data"""
+        return []
 
     @abstractmethod
     def run(self, commit: Commit):
