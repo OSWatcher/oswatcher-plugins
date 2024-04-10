48ad9ca85f9bd8f2105c87dc919158451baaee33
Mathieu Tarral <mathieu.tarral@protonmail.com>
2024-04-10 02:06:53 +0200

    main: add post_mortem

--- plugins/__main__.py
+++ plugins/__main__.py
@@ -1,9 +1,27 @@
 import logging
+from functools import wraps
 
 import click
 from neogit.model.neo import Commit
 
 from plugins.plugins import MAP_PLUGIN, PluginType
+
+
+def post_mortem(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        try:
+            return f(*args, **kwargs)
+        except Exception:
+            logging.exception("Post Mortem: An unhandled exception occurred.")
+            import sys
+
+            import ipdb
+
+            _, _, tb = sys.exc_info()
+            ipdb.post_mortem(tb)
+
+    return wrapper
 
 
 def setup_logging(debug_enabled: bool):
@@ -19,6 +37,7 @@ def setup_logging(debug_enabled: bool):
 @click.option("--debug", "-d", is_flag=True, help="Toggle debug output")
 @click.argument("commit_hash")
 @click.argument("plugin_type_str")
+@post_mortem
 def runner(debug, commit_hash: str, plugin_type_str: str):
     """Plugin Runner"""
     setup_logging(debug)
